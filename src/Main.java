fe30adabbe2e9dcb907690d6a8565b447cedd626
Christian Diesner <chr.diesner@mailbox.org>
2023-04-18 16:50:40 +0200

    while loop included

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,23 @@ public class Main {
         int input;
         System.out.println("Welche Zahl möchtest du faktorisieren?:");
         input = scanner.nextInt();
-        System.out.println("Das Ergebnis von " + input + "! = " + getFactorial(input));
+        System.out.println("Das Ergebnis von " + input + "! = " + getFactorialWhile(input));
     }
 
     public static int getFactorial(int f) {
         int result = 1;
         for (int i = 1; i <= f; i++) {
             result = result * i;
+        }
+        return result;
+    }
+
+    public static int getFactorialWhile(int f) {
+        int result = 1;
+        int i =1;
+        while (i <= f) {
+            result = result * i;
+            i++;
         }
         return result;
     }
